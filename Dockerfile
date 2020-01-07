82a2da5e972c3078858a55566d7b874abc447138
jonny rhea <jonathan.rhea@gmail.com>
2020-01-07 16:14:15 -0600

    adding .gitignore, gen-nutch-patch.sh now checks to see if dir exists, new patch

--- Dockerfile
+++ Dockerfile
@@ -12,13 +12,15 @@ RUN echo 'export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64' >> $HOME/.bashrc
 
 # Download nutch
 RUN curl -o apache-nutch-1.16-bin.tar.gz http://apache.claz.org/nutch/1.16/apache-nutch-1.16-bin.tar.gz \
-    && tar xvzf apache-nutch-1.16-bin.tar.gz \
-    && curl -o apache-nutch-1.16-src.tar.gz http://apache.claz.org/nutch/1.16/apache-nutch-1.16-src.tar.gz \
+    && curl -o apache-nutch-1.16-src.tar.gz http://apache.claz.org/nutch/1.16/apache-nutch-1.16-src.tar.gz
+
+# Extract nutch
+RUN tar xvzf apache-nutch-1.16-bin.tar.gz \
     && mkdir -p apache-nutch-1.16/src \
     && tar xvzf apache-nutch-1.16-src.tar.gz -C apache-nutch-1.16/src
 
 # Download nutch.patch from repo
-RUN curl -o nutch.patch https://raw.githubusercontent.com/ethsearch/config/master/nutch.patch
+RUN curl -o nutch.patch https://raw.githubusercontent.com/ethsearch/config/master/nutch.patch 
 
 # Apply nutch.patch 
 RUN cd apache-nutch-1.16 && patch -p2 <../nutch.patch 
--- gen-nutch-patch.sh
+++ gen-nutch-patch.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
-mkdir -p apache-nutch-1.16-orig/src
+if [ ! -d "apache-nutch-1.16" ]; then
+    echo "You don't have a local copy of Apache Nutch configured for ethsear.ch"
+    exit 1
+fi
 
-# download nutch
-curl -o apache-nutch-1.16-bin.tar.gz http://apache.claz.org/nutch/1.16/apache-nutch-1.16-bin.tar.gz \
-    && tar xvzf apache-nutch-1.16-bin.tar.gz -C apache-nutch-1.16-orig \
-    && curl -o apache-nutch-1.16-src.tar.gz http://apache.claz.org/nutch/1.16/apache-nutch-1.16-src.tar.gz \
-    && tar xvzf apache-nutch-1.16-src.tar.gz -C apache-nutch-1.16-orig/src
+# if a fresh copy of apache-nutch doesn't exist go ahead and download it
+if [ ! -d "apache-nutch-1.16-orig" ]; then
+    mkdir -p apache-nutch-1.16-orig/src
+    curl -o apache-nutch-1.16-bin.tar.gz http://apache.claz.org/nutch/1.16/apache-nutch-1.16-bin.tar.gz \
+        && tar xvzf apache-nutch-1.16-bin.tar.gz -C apache-nutch-1.16-orig \
+        && curl -o apache-nutch-1.16-src.tar.gz http://apache.claz.org/nutch/1.16/apache-nutch-1.16-src.tar.gz \
+        && tar xvzf apache-nutch-1.16-src.tar.gz -C apache-nutch-1.16-orig/src
+fi
 
 # generate a patch by comparing existing nutch folder to the newly downloaded version
 diff -Naur -x crawl -x *.html -x *.jar -x *ivy -x *logs -x src apache-nutch-1.16-orig apache-nutch-1.16 > nutch.patch
